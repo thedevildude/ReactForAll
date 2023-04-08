34442f8fa712b37f6162f3dd2a6de60f8ea4a232
Devdeep Ghosh <devarmforce01@gmail.com>
2023-04-08 15:39:56 +0530

    worked on L1 suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Header from "./Header";
 import ReactForm from "./ReactForm";
 
 const formFields = [
-  {  id:1, label:"First Name", type:"text" },
-  {  id:2, label:"Last Name", type:"text"},
-  {  id:3, label:"Email", type:"email"},
-  {  id:4, label:"Date of Birth", type:"date"},
-  {  id:5, label:"Phone number", type:"number"}
+  { id: 1, label: "First Name", type: "text" },
+  { id: 2, label: "Last Name", type: "text" },
+  { id: 3, label: "Email", type: "email" },
+  { id: 4, label: "Date of Birth", type: "date" },
+  { id: 5, label: "Phone number", type: "tel" },
 ];
 
 function App() {
@@ -16,7 +16,6 @@ function App() {
       <div className="p-4 mx-auto bg-white shadow-lg rounded-xl">
         <Header title="Welcome to Lesson 5 of $react-typescript with #tailwindcss" />
         <ReactForm formFields={formFields} />
-        <button className="py-2 px-5 mt-2 text-white bg-blue-500 font-semibold rounded-lg">Submit</button>
       </div>
     </AppContainer>
   );
--- src/ReactForm.tsx
+++ src/ReactForm.tsx
@@ -1,29 +1,32 @@
-import React from "react"
+import React from "react";
 
 interface Props {
-  formFields: Field[]
+  formFields: Field[];
 }
 
 interface Field {
-  id: number,
-  label: string,
-  type: string
+  id: number;
+  label: string;
+  type: string;
 }
 
 const ReactForm = (props: Props) => {
   return (
     <>
-    {props.formFields.map((field: Field) => (
-      <div className="font-semibold" key={field.id}>
-        <label>{field.label}</label>
-        <input
-          className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
-          type={field.type}
-        />
-      </div>
-    ))}
+      {props.formFields.map((field: Field) => (
+        <div className="font-semibold" key={field.id}>
+          <label>{field.label}</label>
+          <input
+            className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
+            type={field.type}
+          />
+        </div>
+      ))}
+      <button className="py-2 px-5 mt-2 text-white bg-blue-500 font-semibold rounded-lg">
+        Submit
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default ReactForm
+export default ReactForm;
