5fa102bd514812ac6c5a190ea4baf25332f5032b
Devdeep Ghosh <devarmforce01@gmail.com>
2023-04-19 16:30:58 +0530

    prettier used in about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,7 @@
-import React from "react"
+import React from "react";
 
 const About = () => {
-  return (
-    <div>About Page</div>
-  )
-}
+  return <div>About Page</div>;
+};
 
-export default About
+export default About;
