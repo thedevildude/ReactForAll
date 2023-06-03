3e3e6277663889899a09eb43d12aa0efcf2e04f2
Devdeep Ghosh <devarmforce01@gmail.com>
2023-06-03 12:39:53 +0530

    pwa

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,26 +1,35 @@
+import React from "react";
 import { useRoutes, Redirect } from "raviger";
 import About from "../components/About";
 import AppContainer from "../components/AppContainer";
 import ReactForm from "../components/ReactForm";
-import Home from "../components/Home";
 import PreviewForm from "../components/PreviewForm";
 import Login from "../components/Login";
 import { User } from "../types/userTypes";
 import Submissions from "../components/Submissions";
+const Home = React.lazy(() => import("../components/Home"));
 
 const routes = {
-  "/": () => <Home />,
+  "/": () => (
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <Home />
+    </React.Suspense>
+  ),
   "/about": () => <About />,
   "/login": () => <Login />,
   "/forms/:id": ({ id }: { id: string }) => <ReactForm id={Number(id)} />,
   "/preview/:formId": ({ formId }: { formId: string }) => (
     <PreviewForm formId={Number(formId)} />
   ),
-  "/submissions/:id": ({ id }: { id: string }) => <Submissions id={Number(id)} />,
+  "/submissions/:id": ({ id }: { id: string }) => (
+    <Submissions id={Number(id)} />
+  ),
   "*": () => <Redirect to="/" />,
 };
 
 export default function AppRouter(props: { currentUser: User }) {
   let routeResult = useRoutes(routes);
-  return <AppContainer currentUser={props.currentUser}>{routeResult}</AppContainer>;
+  return (
+    <AppContainer currentUser={props.currentUser}>{routeResult}</AppContainer>
+  );
 }
